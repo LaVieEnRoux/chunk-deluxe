6837cdc6b1e0a895c1eecbc470da6bfb10d336b5
Jon Smith <jon.smith@colibri-software.com>
2016-12-06 20:37:21 -0800

    Initial version of BLEU calculation

--- project/main.py
+++ project/main.py
@@ -1,5 +1,9 @@
 from decode_FULL import decode
 from bleu import bleu, bleu_stats
+import numpy as np
+
+numSentences = 100
+bleuNum = 10  # number of bleu stats
 
 # open reference file
 referenceFile = "./data/all.cn-en.en0"
@@ -7,15 +11,18 @@ f = open(referenceFile, "r")
 
 sourceFile1 = "./data/all.cn-en.cn"
 
-for nbest in decode(sourceFile1, numSentences=2):
+allStats = np.empty((numSentences, bleuNum))
+
+for i, nbest in enumerate(decode(sourceFile1, numSentences=numSentences)):
+
+    if i % 50 == 0:
+        print "Decoding sentence {} out of {}...".format(i, numSentences)
 
     ref = f.readline().strip()
 
-    print "Reference:"
-    print ref
+    stats = [el for el in bleu_stats(nbest[0], ref)]
+    stats = np.array(stats)
+    allStats[i, :] = stats
 
-    print
-    print "Translations:"
-    for trns in nbest:
-        print trns
-    print
+bleuScore = bleu(np.mean(allStats, axis=0))
+print "BLEU score: {}".format(bleuScore)
